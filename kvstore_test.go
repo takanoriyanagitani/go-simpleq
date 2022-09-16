cb1cf26a1c3cb7da2f708def8f0cc957f43a4340
yanagitani <takanori.yanagitani@gmail.com>
2022-09-16 16:28:56 +0900

    test: more coverage

---
+++ kvstore_test.go
@@ -0,0 +1,25 @@
+package simpleq
+
+import (
+	"testing"
+)
+
+func TestKvstore(t *testing.T){
+	t.Parallel()
+
+	t.Run("KvStoreBuilder", func(t *testing.T){
+		t.Parallel()
+
+		t.Run("Build", func(t *testing.T){
+			t.Parallel()
+
+			t.Run("invalid", func(t *testing.T){
+				t.Parallel()
+
+				var b KvStoreBuilder = KvStoreBuilderNew()
+				var ek Either[KvStore, error] = b.Build()
+				t.Run("Must invalid", checker(ek.IsNg(), true))
+			})
+		})
+	})
+}
